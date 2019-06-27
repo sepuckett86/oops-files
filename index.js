8b70ce1589ccaa57b761ae8b9bb97202e110b3b4
sepuckett86 <sepuckett86@gmail.com>
2019-06-27 09:38:08 -0700

    clean up index.js

--- index.js
+++ index.js
@@ -8,35 +8,25 @@ const {
 } = require('./file-methods');
 
 function renameAllFiles(directory, callback) {
-  // get array of files in directory
   readDirectory('fixtures', (err, files) => {
     if(err) return callback(err);
-    // loop through array for each file
     let count = 0;
     files.forEach(file => {
-      // get content
       getFileContent(`fixtures/${file}`, (err, data) => {
         if(err) return callback(err);
         const content = data;
         getDateModified(`fixtures/${file}`, (err, data) => {
           if(err) return callback(err);
-          // get date modified
           const dateModified = data;
-          //
           const fileName = file.slice(0, -4);
           const newName = `${content}-${fileName}-${dateModified}`;
-          // rename file
           renameFile(`fixtures/${file}`, `fixtures/${newName}`, (err) => {
             if(err) return callback(err);
             count++;
             if(count === files.length) callback(err);
           })
-
         })
       });
-      // delete old file
-      // when the last file is done, invoke callback
-
     })
   });
 }
