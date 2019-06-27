9fbebb6d7dab8c3ab627df203a0772e5b56fc46f
sepuckett86 <sepuckett86@gmail.com>
2019-06-27 09:31:49 -0700

    generate new name for each file

--- index.js
+++ index.js
@@ -9,13 +9,34 @@ const {
 
 function renameAllFiles(directory, callback) {
   // get array of files in directory
-  // loop through array for each file
-    // get content
-    // get date modified
-    // make new file with same content and new file name
-    // delete old file
-    // when the last file is done, invoke callback
-  callback();
+  readDirectory('fixtures', (err, files) => {
+    if(err) return callback(err);
+    // loop through array for each file
+    let count = 0;
+    files.forEach(file => {
+      // get content
+      getFileContent(`fixtures/${file}`, (err, data) => {
+        if(err) return callback(err);
+        const content = data;
+        getDateModified(`fixtures/${file}`, (err, data) => {
+          if(err) return callback(err);
+          const dateModified = data;
+          //
+          const fileName = file.slice(0, -4);
+          const newName = `${content}-${fileName}-${dateModified}`;
+          console.log(newName);
+
+          count++;
+          if(count === files.length) callback(err);
+        })
+      });
+      // get date modified
+      // make new file with same content and new file name
+      // delete old file
+      // when the last file is done, invoke callback
+
+    })
+  });
 }
 
 module.exports = renameAllFiles;
